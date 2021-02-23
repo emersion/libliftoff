27462556db326bbb7f3ebe6bc80db48944e7a99d
Simon Ser <contact@emersion.fr>
2021-02-23 18:10:47 +0100

    Document that liftoff_device_create takes ownership of the FD

--- include/libliftoff.h
+++ include/libliftoff.h
@@ -14,7 +14,8 @@ struct liftoff_layer;
 /**
  * Initialize libliftoff for a DRM node.
  *
- * The node is expected to have DRM_CLIENT_CAP_ATOMIC enabled.
+ * The node is expected to have DRM_CLIENT_CAP_ATOMIC enabled. libliftoff takes
+ * ownership of the file descriptor.
  */
 struct liftoff_device *liftoff_device_create(int drm_fd);
 /**
